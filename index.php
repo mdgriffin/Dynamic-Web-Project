3f1d8d08feaf079858b2186f0afe618b5775a700
Michael Griffin <mdgriffin064@gmail.com>
2016-11-02 17:43:47 +0000

    Added basic PDO usage example

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <?php
-$db = "DynamicProject";
-$user = "root";
-$pass = "root";
-$server = "mysql:host=localhost;dbname=$db";
+$db_database = "DynamicProject";
+$db_user = "root";
+$db_pass = "root";
+$db_connection_string = "mysql:host=localhost;dbname=$db_database";
 
 try {
-	$connection = new PDO ($server, $user, $pass);
-	$connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	$db = new PDO ($db_connection_string, $db_user, $db_pass);
+	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 } catch(PDOException $e) {
 	die(print_r($e));
 }
+
+/*
+
+$count = $db->exec("INSERT INTO Users (forename, surname) VALUES('Michael', 'Griffin')");
+
+*/
+
+$users = $db->query("SELECT * FROM Users");
 
 ?>
 
@@ -21,7 +29,24 @@ try {
 	</head>
 	<body>
 
-		<h2>Hello World</h2>
+		<h2>Users</h2>
+
+		<table>
+			<thead>
+				<th>ID</th>
+				<th>Forename</th>
+				<th>Surname</th>
+			</thead>
+			<tbody>
+				<?php foreach ($users as $user) { ?>
+					<tr>
+						<td><?php echo $user["user_id"]; ?></td>
+						<td><?php echo $user["forename"]; ?></td>
+						<td><?php echo $user["surname"]; ?></td>
+					</tr>
+				<?php } ?>
+			</tbody>
+		</table>
 
 	</body>
 </html>
