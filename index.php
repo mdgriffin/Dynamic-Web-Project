9c6b471258c2b71e9ea5b3108015fff99fc0aacb
Michael Griffin <mdgriffin064@gmail.com>
2016-11-02 17:50:36 +0000

    Moved connection details

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
-$db_database = "DynamicProject";
-$db_user = "root";
-$db_pass = "root";
-$db_connection_string = "mysql:host=localhost;dbname=$db_database";
-
-try {
-	$db = new PDO ($db_connection_string, $db_user, $db_pass);
-	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-} catch(PDOException $e) {
-	die(print_r($e));
-}
+require_once "app/connection.php";
 
 /*
 
