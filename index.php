357b2624f206a7cb7b1b534e3a8d4a41b9a785b7
Michael Griffin <mdgriffin064@gmail.com>
2016-11-02 17:01:02 +0000

    Added PDO connection

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+$db = "DynamicProject";
+$user = "root";
+$pass = "root";
+$server = "mysql:host=localhost;dbname=$db";
+
+try {
+	$connection = new PDO ($server, $user, $pass);
+	$connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+} catch(PDOException $e) {
+	die(print_r($e));
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
