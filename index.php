a66f0745f0cd2e5253b62ad636c8f43dbb743f23
Michael Griffin <mdgriffin064@gmail.com>
2016-11-03 19:28:27 +0000

    Fetching all hotels

--- index.php
+++ index.php
@@ -16,10 +16,12 @@ $users = $db->query("SELECT * FROM Users");
 //$malton = new Hotel("Malton", "Killarney, Co. Kerry", "Killarney National Park just a short stroll away", 52.059935, -9.504427);
 //$malton->save();
 
-$gleneagle = new Hotel("Gleneagle", "Mucross Road Killarney, Co.Kerry", "Killarney's premier events hotel", 39.045270, -104.824423);
-$gleneagle->save();
-$gleneagle->setDescription("Killarey's finest music and events destination");
-$gleneagle->update();
+//$gleneagle = new Hotel("Gleneagle", "Mucross Road Killarney, Co.Kerry", "Killarney's premier events hotel", 39.045270, -104.824423);
+//$gleneagle->save();
+//$gleneagle->setDescription("Killarey's finest music and events destination");
+//$gleneagle->update();
+
+$hotels = Hotel::getAll();
 
 ?>
 
@@ -31,20 +33,26 @@ $gleneagle->update();
 	</head>
 	<body>
 
-		<h2>Users</h2>
+		<h2>Hotels</h2>
 
 		<table>
 			<thead>
 				<th>ID</th>
-				<th>Forename</th>
-				<th>Surname</th>
+				<th>Name</th>
+				<th>Address</th>
+				<th>Description</th>
+				<th>Latitude</th>
+				<th>longitude</th>
 			</thead>
 			<tbody>
-				<?php foreach ($users as $user) { ?>
+				<?php foreach ($hotels as $hotel) { ?>
 					<tr>
-						<td><?php echo $user["user_id"]; ?></td>
-						<td><?php echo $user["forename"]; ?></td>
-						<td><?php echo $user["surname"]; ?></td>
+						<td><?php echo $hotel["hotel_id"]; ?></td>
+						<td><?php echo $hotel["name"]; ?></td>
+						<td><?php echo $hotel["address"]; ?></td>
+						<td><?php echo $hotel["description"]; ?></td>
+						<td><?php echo $hotel["latitude"]; ?></td>
+						<td><?php echo $hotel["longitude"]; ?></td>
 					</tr>
 				<?php } ?>
 			</tbody>
