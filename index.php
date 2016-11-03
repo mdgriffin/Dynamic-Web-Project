a058f4b63b1aa08981eb27e7aebb3bce763228fe
Michael Griffin <mdgriffin064@gmail.com>
2016-11-03 17:44:09 +0000

    Updated to use new model

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require_once "app/connection.php";
+require_once "app/models/hotel.php";
 
 /*
 
@@ -8,6 +9,17 @@ $count = $db->exec("INSERT INTO Users (forename, surname) VALUES('Michael', 'Gri
 */
 
 $users = $db->query("SELECT * FROM Users");
+
+//$brehon = new Hotel("Brehon", "Killarney, Co. Kerry", "Luxury Hotel in the heart of Killarney", 52.059935, -9.504427);
+//$brehon->save();
+
+//$malton = new Hotel("Malton", "Killarney, Co. Kerry", "Killarney National Park just a short stroll away", 52.059935, -9.504427);
+//$malton->save();
+
+$gleneagle = new Hotel("Gleneagle", "Mucross Road Killarney, Co.Kerry", "Killarney's premier events hotel", 39.045270, -104.824423);
+$gleneagle->save();
+$gleneagle->setDescription("Killarey's finest music and events destination");
+$gleneagle->update();
 
 ?>
 
