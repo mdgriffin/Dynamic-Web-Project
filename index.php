3c5d199577fd336395d983883cb5e87de0794059
Michael Griffin <mdgriffin064@gmail.com>
2016-11-04 10:08:51 +0000

    Modified variable names from Hotel to Venue

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require_once "app/connection.php";
-require_once "app/models/hotel.php";
+require_once "app/models/Venue.php";
 
 /*
 
@@ -8,20 +8,20 @@ $count = $db->exec("INSERT INTO Users (forename, surname) VALUES('Michael', 'Gri
 
 */
 
-$users = $db->query("SELECT * FROM Users");
+//$users = $db->query("SELECT * FROM Users");
 
-//$brehon = new Hotel("Brehon", "Killarney, Co. Kerry", "Luxury Hotel in the heart of Killarney", 52.059935, -9.504427);
+//$brehon = new Venue("Brehon", "Killarney, Co. Kerry", "Luxury Hotel in the heart of Killarney", 52.059935, -9.504427);
 //$brehon->save();
 
-//$malton = new Hotel("Malton", "Killarney, Co. Kerry", "Killarney National Park just a short stroll away", 52.059935, -9.504427);
+//$malton = new Venue("Malton", "Killarney, Co. Kerry", "Killarney National Park just a short stroll away", 52.059935, -9.504427);
 //$malton->save();
 
-//$gleneagle = new Hotel("Gleneagle", "Mucross Road Killarney, Co.Kerry", "Killarney's premier events hotel", 39.045270, -104.824423);
+//$gleneagle = new Venue("Gleneagle", "Mucross Road Killarney, Co.Kerry", "Killarney's premier events hotel", 39.045270, -104.824423);
 //$gleneagle->save();
 //$gleneagle->setDescription("Killarey's finest music and events destination");
 //$gleneagle->update();
 
-$hotels = Hotel::getAll();
+$venues = Venue::getAll();
 //$hotel_single = Hotel::get(10);
 
 $pageTitle = "Find the perfect venue";
@@ -42,14 +42,14 @@ include_once("partials/header.php");
 		<th>longitude</th>
 	</thead>
 	<tbody>
-		<?php foreach ($hotels as $hotel) { ?>
+		<?php foreach ($venues as $venue) { ?>
 			<tr>
-				<td><?php echo $hotel["hotel_id"]; ?></td>
-				<td><?php echo $hotel["name"]; ?></td>
-				<td><?php echo $hotel["address"]; ?></td>
-				<td><?php echo $hotel["description"]; ?></td>
-				<td><?php echo $hotel["latitude"]; ?></td>
-				<td><?php echo $hotel["longitude"]; ?></td>
+				<td><?php echo $venue["venue_id"]; ?></td>
+				<td><?php echo $venue["name"]; ?></td>
+				<td><?php echo $venue["address"]; ?></td>
+				<td><?php echo $venue["description"]; ?></td>
+				<td><?php echo $venue["latitude"]; ?></td>
+				<td><?php echo $venue["longitude"]; ?></td>
 			</tr>
 		<?php } ?>
 	</tbody>
