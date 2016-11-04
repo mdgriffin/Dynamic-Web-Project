18f89110468fd2124371c7e90cb1f749670bb11d
Michael Griffin <mdgriffin064@gmail.com>
2016-11-04 19:55:58 +0000

    Added proper validation logic

--- app/Validator.php
+++ app/Validator.php
@@ -21,43 +21,57 @@ class Validator {
 	}
 
 	public function isNumeric () {
-		$this->errors["isNumeric"] = $this->error_messages["isNumeric"];
+		if (!ctype_digit($this->value)) {
+			$this->errors["isNumeric"] = $this->error_messages["isNumeric"];
+		}
 
 		return $this;
 	}
 
 	public function isAlpha () {
-		$this->errors["isAlpha"] = $this->error_messages["isAlpha"];
+		if (!ctype_alpha($this->value)) {
+			$this->errors["isAlpha"] = $this->error_messages["isAlpha"];
+		}
 
 		return $this;
 	}
 
 	public function isAlphaNumeric () {
-		$this->errors["isAlphaNumeric"] = $this->error_messages["isAlphaNumeric"];
+		if (!ctype_alnum($this->value)) {
+			$this->errors["isAlphaNumeric"] = $this->error_messages["isAlphaNumeric"];
+		}
 
 		return $this;
 	}
 
-	public function isMinLength ($length) {
-		$this->errors["isMinLength"] = $this->error_messages["isMinLength"];
+	public function isMinLength ($min_length) {
+		if (strlen($this->value) < $min_length) {
+			$this->errors["isMinLength"] = $this->error_messages["isMinLength"];
+		}
 
 		return $this;
 	}
 
-	public function isMaxLength ($length) {
-		$this->errors["isMaxLength"] = $this->error_messages["isMaxLength"];
+	public function isMaxLength ($max_length) {
+		if (strlen($this->value) > $max_length) {
+			$this->errors["isMaxLength"] = $this->error_messages["isMaxLength"];
+		}
 
 		return $this;
 	}
 
 	public function isEmpty () {
-		$this->errors["isEmpty"] = $this->error_messages["isEmpty"];
+		if (!strlen($this->value)) {
+			$this->errors["isEmpty"] = $this->error_messages["isEmpty"];
+		}
 
 		return $this;
 	}
 
 	public function isEmail () {
-		$this->errors["isEmail"] = $this->error_messages["isEmail"];
+		if (!filter_var($email_a, FILTER_VALIDATE_EMAIL)) {
+			$this->errors["isEmail"] = $this->error_messages["isEmail"];
+		}
 
 		return $this;
 	}
@@ -67,7 +81,10 @@ class Validator {
 	}
 
 	public function isValid () {
-		return false;
+		if (count($this->errors)) {
+			return false;
+		}
+		return true;
 	}
 
 }
