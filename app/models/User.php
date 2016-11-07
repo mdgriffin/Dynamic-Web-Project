1f0c5ef02a68ae82a7aa0cb099e496b18e7dfc3d
Michael Griffin <mdgriffin064@gmail.com>
2016-11-07 20:57:52 +0000

    Created model for user

---
+++ app/models/User.php
@@ -0,0 +1,47 @@
+<?php
+class	Venue {
+	use Model;
+
+	private $id;
+	private $forename;
+	private $surname;
+	private $email;
+	private $telephone;
+	private $password;
+
+	public function __construct ($forename, $surname, $email, $telephone, $password) {
+		$this->setForename($forename);
+		$this->serSurname($surname);
+		$this->setEmail($email);
+		$this->setTelephone($telephone);
+		$this->setPassword();
+	}
+
+	public function setPassword ($password) {
+		// hash the password
+	}
+
+	public function errors () {
+		$errors = array();
+
+		return $errors;
+	}
+
+	private function _save () {
+	}
+
+	private function _update () {
+
+	}
+
+	private static function _getAll ($arg) {
+
+	}
+
+	private static function _get($id) {
+
+	}
+
+}
+
+?>
