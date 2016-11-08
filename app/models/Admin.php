e66389ae75d3d9376b81e4752d808c15f0d45537
Michael Griffin <mdgriffin064@gmail.com>
2016-11-08 19:59:02 +0000

    Added password hashing and insert statement for Admin

--- app/models/Admin.php
+++ app/models/Admin.php
@@ -15,8 +15,16 @@ class	 Admin {
 		$this->setPassword();
 	}
 
+	/**
+	 * Example of good Password Security found here: https://alias.io/2010/01/store-passwords-safely-with-php-and-mysql/
+	 **/
+
 	public function setPassword ($password) {
-		// hash the password
+		$cost = 10;
+		$salt = strtr(base64_encode(mcrypt_create_iv(16, MCRYPT_DEV_URANDOM)), '+', '.');
+		$salt = sprintf("$2a$%02d$", $cost) . $salt;
+
+		$this->password = crypt($password, $salt);
 	}
 
 	public static function isAdmin ($username, $password) {
@@ -29,8 +37,17 @@ class	 Admin {
 		return $errors;
 	}
 
-
 	private function _save () {
+		$insert_stmt = self::$db->prepare("INSERT INTO Admins (forename, surname, email, password) VALUES(:forename, :surname, :email, :password)");
+
+		$insert_stmt->execute(array(
+			':forename' => $this->forename,
+			':surname' => $this->surname,
+			':email' => $this->email,
+			':password' => $this->password
+		));
+
+		$this->id = self::$db->lastInsertId("admin_id");
 	}
 
 	private function _update () {
