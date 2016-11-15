9715afd45a0ffdafdd9a989220aea6a1852e70a8
Michael Griffin <mdgriffin064@gmail.com>
2016-11-15 12:33:14 +0000

    Fixed home page include url

--- index.php
+++ index.php
@@ -4,9 +4,18 @@ require_once "app/connection.php";
 require_once "app/models/Model.php";
 require_once "app/Validator.php";
 
-include_once("../partials/header.php");
+$pageTitle = "Find the Perfect Venue";
+
+include_once("partials/header.php");
 
 ?>
+
+<nav>
+	<a href="venues">Venues</a>
+	<a href="admin">Admin</a>
+</nav>
+
+<h1>Home Page</h1>
 
 
 
