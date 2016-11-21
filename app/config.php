feb42d71db72af5cc3c039eec5cc7d2dbfc90ea0
Michael Griffin <mdgriffin064@gmail.com>
2016-11-21 20:37:46 +0000

    Changed database connection to use AWS

--- app/config.php
+++ app/config.php
@@ -1,9 +1,9 @@
 <?php
 
 $config = array(
-	"db_user" => "root",
-	"db_pass" => "root",
-	"db_connection_string" => "mysql:host=localhost;dbname=DynamicProject",
+	"db_user" => "mdgriffin",
+	"db_pass" => "w8qqptj9",
+	"db_connection_string" => "mysql:host=dynamic-web-mariadb.cb3rcz9pp5kx.eu-west-1.rds.amazonaws.com:3306;dbname=DynamicProject",
 	"base_url" => "\/Dynamic-Web-Project\/"
 );
 
