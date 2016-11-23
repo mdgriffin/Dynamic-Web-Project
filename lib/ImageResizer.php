0129b152304f3b3da1006333d680437908e49941
Michael Griffin <mdgriffin064@gmail.com>
2016-11-23 20:19:33 +0000

    Added basic image resizing

--- lib/ImageResizer.php
+++ lib/ImageResizer.php
@@ -2,40 +2,42 @@
 
 class ImageResizer {
 
-  private static $target_dir = "Assets/temp";
-  private static $sizes = array(
-    "banner" => array(900, 450),
-    "large_square" => array(900, 900),
-    "medium_square" => array(600, 600),
-    "thumbnail" => array(150, 150)
-  );
+	private static $target_dir = "Assets/temp_images/";
+	private static $sizes = array(
+		"banner" => array(900, 450),
+		"large_square" => array(900, 900),
+		"medium_square" => array(600, 600),
+		"thumbnail" => array(150, 150)
+	);
 
-  public static function getImage ($imageSrc, $sizeKey) {
+	public static function getImage ($imageSrc, $sizeKey) {
 
-    $imageSrc_parts = pathinfo($imageSrc);
-    $imageSrcName = $imageSrc_parts["filename"];
-    $imageSrcExt = $imageSrc_parts["extension"];
+		$imageSrc_parts = pathinfo($imageSrc);
+		$imageSrcName = $imageSrc_parts["filename"];
+		$imageSrcExt = $imageSrc_parts["extension"];
 
-    $imageSrcExt = $imageSrc;
-    $target_file_src =  self::$target_dir . $imageSrcName . "_" . $sizeKey . "." . $imageSrcExt;
+		$target_file_src =  self::$target_dir . $imageSrcName . "_" . $sizeKey . "." . $imageSrcExt;
 
-    // check if the image has already been generated
-    if (file_exists($target_file_src)) {
-      return $target_file_src;
-    // if not create the new image
-    } else {
-      /*
-      $dest_image = imagecreatetruecolor($sizes[$sizeKey][0], $sizes[$sizeKey][1]);
-      $src_image = imagecreatefromjpeg($imageSrc);
-      list($src_width, $src_height) = getimagesize($imageSrc);
-      imagecopyresampled($dest_image, $src_image, 0, 0, 0, 0, $sizes[$sizeKey][0], $sizes[$sizeKey][1], $src_width, $src_height);
-      */
-      // temp !!!
-      return $imageSrc;
-    }
+		// check if the image has already been generated
+		if (file_exists($target_file_src)) {
+			return $target_file_src;
+		// if not create the new image
+		} else {
+			$dest_image = imagecreatetruecolor(self::$sizes[$sizeKey][0], self::$sizes[$sizeKey][1]);
+			$src_image = imagecreatefromjpeg($imageSrc);
+			list($src_width, $src_height) = getimagesize($imageSrc);
+			imagecopyresampled($dest_image, $src_image, 0, 0, 0, 0, self::$sizes[$sizeKey][0], self::$sizes[$sizeKey][1], $src_width, $src_height);
 
-  }
+			// !!! check the file type and call the correct method
+			imagejpeg($dest_image, $target_file_src);
+			imagedestroy($dest_image);
+
+			// return the url of the newly generated image
+			return $target_file_src;
+		}
+
+	}
+
 }
-
 
 ?>
