ec5bcc116272d405ebe8148b88e611da6d267ac7
Michael Griffin <mdgriffin064@gmail.com>
2016-11-24 21:53:08 +0000

    Improved image cropping

--- app/views/admin/images.php
+++ app/views/admin/images.php
@@ -42,9 +42,9 @@ include_once("app/views/partials/admin-nav.php");
       <tr>
           <td><?php echo $venue_image["image_id"]; ?></td>
           <td>
-						<h1><?php echo Image::getSize($venue_image['source'], 'large_square'); ?>"</h1>
+						<h1><?php echo Image::getSize($venue_image['source'], 'thumbnail'); ?>"</h1>
             <a href="<?php echo $venue_image["source"]; ?>">
-              <img src="<?php echo Image::getSize($venue_image['source'], 'large_square'); ?>" alt="<?php echo $venue_image["title"]; ?>" title="<?php echo $venue_image["title"]; ?>">
+              <img src="<?php echo Image::getSize($venue_image['source'], 'thumbnail'); ?>" alt="<?php echo $venue_image["title"]; ?>" title="<?php echo $venue_image["title"]; ?>">
             <a/>
         </td>
       </tr>
--- lib/Image.php
+++ lib/Image.php
@@ -16,7 +16,7 @@ class Image {
 		$imageSrcName = $imageSrc_parts["filename"];
 		$imageSrcExt = $imageSrc_parts["extension"];
 
-		$target_file_src =  self::$target_dir . $imageSrcName . "_" . $sizeKey . "." . $imageSrcExt;
+		$target_file_src =  self::$target_dir . $imageSrcName . "_" . $sizeKey . "2." . $imageSrcExt;
 
 		// check if the image has already been generated
 		if (file_exists($target_file_src)) {
@@ -28,8 +28,9 @@ class Image {
 
 			list($src_width, $src_height) = getimagesize($imageSrc);
 			list($srcX, $srcY) = self::getSourceXY(self::$sizes[$sizeKey][0], self::$sizes[$sizeKey][1], $src_width, $src_height);
+			list($normalizedSrcWidth, $normalizedSrcHeight) = self::getNormalizedSize(self::$sizes[$sizeKey][0], self::$sizes[$sizeKey][1], $src_width, $src_height);
 
-			imagecopyresampled($dest_image, $src_image, 0, 0, $srcX, $srcY, self::$sizes[$sizeKey][0], self::$sizes[$sizeKey][1], $src_width, $src_height);
+			imagecopyresampled($dest_image, $src_image, 0, 0, $srcX, $srcY, self::$sizes[$sizeKey][0], self::$sizes[$sizeKey][1], $normalizedSrcWidth, $normalizedSrcHeight);
 
 			if ($imageSrcExt == "png") {
 				imagepng($dest_image, $target_file_src);
@@ -47,11 +48,29 @@ class Image {
 
 	}
 
+	private static function getNormalizedSize ($destWidth, $destHeight, $srcWidth, $srcHeight) {
+
+			// if width is closer
+			if (abs($destWidth - $srcWidth) < abs($destHeight - $srcHeight)) {
+				$srcHeight = $destHeight * ($srcWidth / $destWidth);
+			// else height is closer
+		} else {
+				$srcWidth = $destWidth * ($srcHeight / $destHeight);
+		}
+
+		return array($srcWidth, $srcHeight);
+	}
+
 	private static function getSourceXY($destWidth, $destHeight, $srcWidth, $srcHeight) {
-		// todo Figure out how the source image into the destination image in the most efficient fashion
-		// finding the x and y co-ordinates  of the src image
-		$x = 0;
-		$y = 0;
+		list($normalizedSrcWidth, $normalizedSrcHeight) = self::getNormalizedSize($destWidth, $destHeight, $srcWidth, $srcHeight);
+
+		if ($normalizedSrcWidth != $srcWidth) {
+			$x = abs($srcWidth - $normalizedSrcWidth) / 2;
+			$y = 0;
+		} else {
+			$x = 0;
+			$y = abs($srcHeight - $normalizedSrcHeight) / 2;
+		}
 
 		return array($x, $y);
 	}
