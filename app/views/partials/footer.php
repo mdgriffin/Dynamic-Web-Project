3a4070f39c825afea91174d73586984be830ec28
Michael Griffin <mdgriffin064@gmail.com>
2016-11-26 14:26:15 +0000

    Improved header styling

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -1,3 +1,4 @@
+	</div><!-- .container -->
 	<script type="text/javascript" src="Assets/script.js"></script>
 </body>
 </html>
--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -5,7 +5,7 @@
 		<title><?php echo $this->pageTitle; ?></title>
 		<base href="/Dynamic-Web-Project/">
 
-		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Nunito|Open+Sans" rel="stylesheet">
 		<link rel="stylesheet" href="assets/style.css">
 	</head>
 	<body>
@@ -19,41 +19,57 @@
 		<?php } ?>
 
 		<header>
-				<h1>VenYou</h1>
+				<div class="header-topBar">
 
-				<form action="search" method="get">
-					<input type="text" name="location" value="" id="searchInput" placeholder="location">
+					<h1>VenYou</h1>
 
-					<label for="num_guests">Number of guests</label>
-					<select class="" name="num_guests">
-						<option value="0-20">Less than 20</option>
-						<option value="20-40">20-40</option>
-						<option value="40-60">40-60</option>
-						<option value="60-100">60-100</option>
-						<option value="100-150">100-150</option>
-						<option value="150-200">150-200</option>
-						<option value="200-250">200-250</option>
-						<option value="250-500">250-500</option>
-						<option value="500-1000">More than500</option>
-					</select>
-			</form>
+					<form action="search" method="get">
 
-			<div class="datePicker-container">
+						<label for="location">Where</label>
+						<div class="inputWrapper searchWrapper">
+							<input type="text" name="location" value="" class="searchInput" placeholder="Location or venue name">
+						</div>
 
-				<label for="date">Date</label>
-				<input type="text" name="date" value="" id="dateInput" readonly>
+						<label for="num_guests">Number of guests</label>
 
-				<div class="datePicker-dropdown" id="datePicker-dropdown">
-					<div class="datePicker" id="datePicker-header"></div>
+						<div class="inputWrapper selectWrapper">
+							<select class="" name="num_guests">
+								<option value="0-20">Less than 20</option>
+								<option value="20-40">20-40</option>
+								<option value="40-60">40-60</option>
+								<option value="60-100">60-100</option>
+								<option value="100-150">100-150</option>
+								<option value="150-200">150-200</option>
+								<option value="200-250">200-250</option>
+								<option value="250-500">250-500</option>
+								<option value="500-1000">More than500</option>
+							</select>
+						</div>
+				</form>
 
-					<button type="button" class="datePicker-btn" id="datePicker-prev">Prev</button>
-					<button type="button" class="datePicker-btn" id="datePicker-next">Next</button>
+				<div class="datePicker-container">
+
+					<label for="date">Date</label>
+
+					<div class="inputWrapper dateWrapper">
+						<input type="text" name="date" value="" class="dateInput" id="dateInput" readonly>
+					</div>
+
+					<div class="datePicker-dropdown" id="datePicker-dropdown">
+						<div class="datePicker" id="datePicker-header"></div>
+
+						<button type="button" class="datePicker-btn" id="datePicker-prev"><span class="icon-left-open-big"></span></button>
+						<button type="button" class="datePicker-btn" id="datePicker-next"><span class="icon-right-open-big"></span></button>
+					</div>
+
 				</div>
 
 			</div>
-
-
+			<div class="headerNavBar">
 				<nav>
 
 				</nav>
+			</div>
 		</header>
+
+		<div class="container">
