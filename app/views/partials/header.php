5b11b1f8d0b22ca95e4e55121450d2e3a73646c2
Michael Griffin <mdgriffin064@gmail.com>
2016-11-26 15:58:12 +0000

    Improved header navigation styling

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -19,57 +19,81 @@
 		<?php } ?>
 
 		<header>
+
 				<div class="header-topBar">
 
-					<h1>VenYou</h1>
+					<div class="container">
 
-					<form action="search" method="get">
+						<h1>VenYou</h1>
 
-						<label for="location">Where</label>
-						<div class="inputWrapper searchWrapper">
-							<input type="text" name="location" value="" class="searchInput" placeholder="Location or venue name">
+						<form action="search" method="get">
+
+							<label for="location">Where</label>
+							<div class="inputWrapper searchWrapper">
+								<input type="text" name="location" value="" class="searchInput" placeholder="Location or venue name">
+							</div>
+
+							<label for="num_guests">Number of guests</label>
+
+							<div class="inputWrapper selectWrapper">
+								<select class="" name="num_guests">
+									<option value="0-20">Less than 20</option>
+									<option value="20-40">20-40</option>
+									<option value="40-60">40-60</option>
+									<option value="60-100">60-100</option>
+									<option value="100-150">100-150</option>
+									<option value="150-200">150-200</option>
+									<option value="200-250">200-250</option>
+									<option value="250-500">250-500</option>
+									<option value="500-1000">More than500</option>
+								</select>
+							</div>
+					</form>
+
+					<div class="datePicker-container">
+
+						<label for="date">Date</label>
+
+						<div class="inputWrapper dateWrapper">
+							<input type="text" name="date" value="" class="dateInput" id="dateInput" readonly>
 						</div>
 
-						<label for="num_guests">Number of guests</label>
+						<div class="datePicker-dropdown" id="datePicker-dropdown">
+							<div class="datePicker" id="datePicker-header"></div>
 
-						<div class="inputWrapper selectWrapper">
-							<select class="" name="num_guests">
-								<option value="0-20">Less than 20</option>
-								<option value="20-40">20-40</option>
-								<option value="40-60">40-60</option>
-								<option value="60-100">60-100</option>
-								<option value="100-150">100-150</option>
-								<option value="150-200">150-200</option>
-								<option value="200-250">200-250</option>
-								<option value="250-500">250-500</option>
-								<option value="500-1000">More than500</option>
-							</select>
+							<button type="button" class="datePicker-btn" id="datePicker-prev"><span class="icon-left-open-big"></span></button>
+							<button type="button" class="datePicker-btn" id="datePicker-next"><span class="icon-right-open-big"></span></button>
 						</div>
-				</form>
 
-				<div class="datePicker-container">
+					</div><!-- datePicker-container -->
 
-					<label for="date">Date</label>
+				</div><!-- container -->
 
-					<div class="inputWrapper dateWrapper">
-						<input type="text" name="date" value="" class="dateInput" id="dateInput" readonly>
-					</div>
+			</div><!-- header-topBar -->
 
-					<div class="datePicker-dropdown" id="datePicker-dropdown">
-						<div class="datePicker" id="datePicker-header"></div>
-
-						<button type="button" class="datePicker-btn" id="datePicker-prev"><span class="icon-left-open-big"></span></button>
-						<button type="button" class="datePicker-btn" id="datePicker-next"><span class="icon-right-open-big"></span></button>
-					</div>
-
-				</div>
-
-			</div>
 			<div class="headerNavBar">
-				<nav>
 
-				</nav>
-			</div>
+				<div class="container">
+
+					<nav>
+						<a href="#"><span class="icon-building"></span>Venues</a><!--
+						--><a href="#"><span class="icon-gift"></span>Packages</a><!--
+						--><a href="#"><span class="icon-map-marker"></span>Locations</a><!--
+						--><a href="#"><span class="icon-news"></span>Blog</a><!--
+						--><a href="#"><span class="icon-email"></span>Contact</a>
+					</nav>
+
+					<div class="headerNavBar-account">
+
+						<a href="login"><button type="button">Login</button></a>
+						<a href="register"><button type="button">Register</button></a>
+
+					</div>
+
+				</div><!-- container -->
+
+			</div><!-- headerNavBar -->
+
 		</header>
 
 		<div class="container">
