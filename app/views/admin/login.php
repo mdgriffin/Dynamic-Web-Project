770fcf147d07342ca3a1fb50cc8c4dc09b7cb3ed
Michael Griffin <mdgriffin064@gmail.com>
2016-11-27 12:28:43 +0000

    Improved login form styling

--- app/views/admin/login.php
+++ app/views/admin/login.php
@@ -1,21 +1,35 @@
 <?php include_once("app/views/partials/header.php"); ?>
 
-<h1>Admin Login</h1>
+<div class="gs">
 
-<form action="admin/login.php" method="post">
+	<div class="gs-col gs4 gs4-before">
 
-	<fieldset>
-		<label for="email">Email</label>
-		<input type="text" name="email" value="">
-	</fieldset>
+		<h1>Admin Login</h1>
 
-	<fieldset>
-		<label for="password">Password</label>
-		<input type="text" name="password" value="">
-	</fieldset>
+		<div class="card">
 
-	<input type="submit" name="login" value="Login">
+			<form action="admin/login.php" class="form" method="post">
 
-</form>
+				<fieldset>
+
+					<input type="text" name="email" id="email" value=""><!--
+					--><label for="email" class="form-mainLabel">Email</label>
+				</fieldset>
+
+				<fieldset>
+
+					<input type="password" name="password" id="password" value=""><!--
+					--><label for="password" class="form-mainLabel">Password</label>
+				</fieldset>
+
+				<input type="submit" name="login" value="Login" class="btn btn-primary btn-large">
+
+			</form>
+
+		</div>
+
+	</div>
+
+</div><!-- gs -->
 
 <?php include_once("app/views/partials/footer.php"); ?>
