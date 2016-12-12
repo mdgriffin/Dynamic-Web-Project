78aca308b2b3a3a7737d86bc0d27b05509a1c4c3
Michael Griffin <t00119683@365s.ittralee.ie>
2016-12-12 14:17:48 +0000

    Fixed venue card text overflow issue

--- app/views/venues/card.php
+++ app/views/venues/card.php
@@ -2,6 +2,6 @@
 
 	<img src="/Dynamic-Web-Project/<?php echo Image::getSize($this->image['source'], 'small_banner'); ?>" alt="<?php echo $this->image['title']; ?>" />
 
-<p><?php echo $this->venue->getDescription(); ?></p>
+<p><?php echo substr($this->venue->getDescription(), 0, 200) . "..."; ?></p>
 
 <a href="venues/<?php echo $this->venue->getId(); ?>" class="btn btn-medium btn-secondary btn-fullWidth">View Venue</a>
