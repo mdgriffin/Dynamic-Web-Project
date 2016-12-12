f4ff05e3347f40cb2ddc302108ff95634e0d03d8
Michael Griffin <t00119683@365s.ittralee.ie>
2016-12-12 13:31:34 +0000

    Fixed image resizing

--- lib/Image.php
+++ lib/Image.php
@@ -28,8 +28,11 @@ class Image {
 			$src_image = imagecreatefromjpeg($imageSrc);
 
 			list($src_width, $src_height) = getimagesize($imageSrc);
-			list($srcX, $srcY) = self::getSourceXY(self::$sizes[$sizeKey][0], self::$sizes[$sizeKey][1], $src_width, $src_height);
-			list($normalizedSrcWidth, $normalizedSrcHeight) = self::getNormalizedSize(self::$sizes[$sizeKey][0], self::$sizes[$sizeKey][1], $src_width, $src_height);
+
+			//list($srcX, $srcY) = self::getSourceXY(self::$sizes[$sizeKey][0], self::$sizes[$sizeKey][1], $src_width, $src_height);
+			//list($normalizedSrcWidth, $normalizedSrcHeight) = self::getNormalizedSize(self::$sizes[$sizeKey][0], self::$sizes[$sizeKey][1], $src_width, $src_height);
+
+			list($normalizedSrcWidth, $normalizedSrcHeight,$srcX, $srcY) = self::getBestFit($src_width, $src_height, self::$sizes[$sizeKey][0], self::$sizes[$sizeKey][1]);
 
 			imagecopyresampled($dest_image, $src_image, 0, 0, $srcX, $srcY, self::$sizes[$sizeKey][0], self::$sizes[$sizeKey][1], $normalizedSrcWidth, $normalizedSrcHeight);
 
@@ -49,31 +52,30 @@ class Image {
 
 	}
 
-	private static function getNormalizedSize ($destWidth, $destHeight, $srcWidth, $srcHeight) {
+	private static function getBestFit ($src_width, $src_height, $dest_width, $dest_height) {
+		$width_diff = abs($src_width - $dest_width);
+		$height_diff = abs($src_height - $dest_height);
 
-			// if width is closer
-			if (abs($destWidth - $srcWidth) < abs($destHeight - $srcHeight)) {
-				$srcHeight = $destHeight * ($srcWidth / $destWidth);
-			// else height is closer
+		if ($width_diff > $height_diff) {
+			// scale width
+			$scale_factor = $src_width / $dest_width;
+			$resized_width = $src_width;
+			$resized_height = $dest_height * $scale_factor;
 		} else {
-				$srcWidth = $destWidth * ($srcHeight / $destHeight);
+			// scale height
+			$scale_factor = $src_height / $dest_height;
+			$resized_width = $dest_width * $scale_factor;
+			$resized_height = $src_height;
 		}
 
-		return array($srcWidth, $srcHeight);
-	}
-
-	private static function getSourceXY($destWidth, $destHeight, $srcWidth, $srcHeight) {
-		list($normalizedSrcWidth, $normalizedSrcHeight) = self::getNormalizedSize($destWidth, $destHeight, $srcWidth, $srcHeight);
-
-		if ($normalizedSrcWidth != $srcWidth) {
-			$x = abs($srcWidth - $normalizedSrcWidth) / 2;
-			$y = 0;
+		if ($resized_width > $src_width || $resized_height > $src_height) {
+			return self::getBestFit($src_width, $src_height, $resized_width, $resized_height);
 		} else {
-			$x = 0;
-			$y = abs($srcHeight - $normalizedSrcHeight) / 2;
-		}
+			$srcX = ($src_width - $resized_width) / 2;
+			$srcY = ($src_height - $resized_height) / 2;
 
-		return array($x, $y);
+			return array($resized_width, $resized_height, $srcX, $srcY);
+		}
 	}
 
 }
